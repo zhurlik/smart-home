b1f7d659c36134c5114c4f3daf06ac36a77b6825
zhurlik <zhurlik@gmail.com>
2018-09-10 22:40:40 +0300

    a small fix

--- udp-server/src/test/java/com/zhurlik/smart/home/udp/ApplicationTest.java
+++ udp-server/src/test/java/com/zhurlik/smart/home/udp/ApplicationTest.java
@@ -1,5 +1,6 @@
 package com.zhurlik.smart.home.udp;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.ipc.netty.NettyContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -16,12 +18,25 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * @author zhurlik@gmail.com
  */
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(
+        classes = Application.class,
+        properties = {
+                "server.port=9999"
+        }
+)
 @ActiveProfiles({"test"})
 public class ApplicationTest {
 
     @Autowired
+    private NettyContext netty;
+
+    @Autowired
     private ApplicationContext context;
+
+    @AfterEach
+    void tearDown() {
+        netty.dispose();
+    }
 
     @Test
     void testUp() {
