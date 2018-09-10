19450c81fd8dab243cb4e56e6b961b6e98cf1061
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-09-10 18:43:13 +0300

    very simple udp handler

--- udp-server/src/main/java/com/zhurlik/smart/home/udp/config/UdpServerConfig.java
+++ udp-server/src/main/java/com/zhurlik/smart/home/udp/config/UdpServerConfig.java
@@ -1,10 +1,15 @@
 package com.zhurlik.smart.home.udp.config;
 
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 import reactor.ipc.netty.NettyContext;
+import reactor.ipc.netty.udp.UdpInbound;
+import reactor.ipc.netty.udp.UdpOutbound;
 import reactor.ipc.netty.udp.UdpServer;
 
 /**
@@ -14,15 +19,22 @@ import reactor.ipc.netty.udp.UdpServer;
  */
 @Configuration
 public class UdpServerConfig {
+    private final static Logger LOGGER = LoggerFactory.getLogger(UdpServerConfig.class);
 
     @Bean
     public NettyContext udpServer(final @Value("${server.port}") int port) {
-
-        return UdpServer.create(port)
-                .newHandler((in, out) -> {
-                    // todo: add a handler
-                    return Flux.never();
-                })
+        return UdpServer
+                .create("127.0.0.1", port)
+                .newHandler(this::handler)
                 .block();
+    }
+
+    private Publisher<Void> handler(final UdpInbound in, final UdpOutbound out) {
+        in.receive()
+                .asString()
+                //.log()
+                .subscribe(s -> LOGGER.debug(">> UDP packet:{}", s));
+
+        return Flux.never();
     }
 }
--- udp-server/src/test/java/com/zhurlik/smart/home/udp/config/UdpServerConfigTest.java
+++ udp-server/src/test/java/com/zhurlik/smart/home/udp/config/UdpServerConfigTest.java
@@ -3,14 +3,18 @@ package com.zhurlik.smart.home.udp.config;
 import org.junit.jupiter.api.Test;
 import reactor.ipc.netty.NettyContext;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UdpServerConfigTest {
     private final UdpServerConfig config = new UdpServerConfig();
     @Test
-    void testMain() {
+    void testMain() throws Exception {
         final NettyContext nettyContext = config.udpServer(9999);
         assertNotNull(nettyContext);
+
+        TimeUnit.SECONDS.sleep(15);
         nettyContext.dispose();
     }
 }
