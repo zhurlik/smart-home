f41d47f102ed2454a2a2ab2138d1aa907bf0c040
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-09-10 15:51:20 +0300

    UDP

---
+++ udp-server/src/main/java/com/zhurlik/smart/home/udp/config/UdpServerConfig.java
@@ -0,0 +1,12 @@
+package com.zhurlik.smart.home.udp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Here will be started Netty server for handling UDP protocol.
+ *
+ * @author zhurlik@gmail.com
+ */
+@Configuration
+public class UdpServerConfig {
+}
