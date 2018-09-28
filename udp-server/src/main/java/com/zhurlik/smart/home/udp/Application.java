669ba81f24ebcd1e246c9fe401e14f85e83f3a7a
zhurlik <zhurlik@gmail.com>
2018-09-28 23:00:49 +0300

    loop

--- udp-server/src/main/java/com/zhurlik/smart/home/udp/Application.java
+++ udp-server/src/main/java/com/zhurlik/smart/home/udp/Application.java
@@ -11,7 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
+        Thread.currentThread().join();
     }
 }
