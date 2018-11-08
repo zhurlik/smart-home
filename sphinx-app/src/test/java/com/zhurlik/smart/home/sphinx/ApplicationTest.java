a34b5633d6b719c813dc66fc7e13e29556a79127
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-11-08 16:49:11 +0300

    start point

---
+++ sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/ApplicationTest.java
@@ -0,0 +1,24 @@
+package com.zhurlik.smart.home.sphinx;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Autowired
+    private ApplicationContext spring;
+
+    @Test
+    void testMain() {
+        assertNotNull(spring);
+    }
+}
