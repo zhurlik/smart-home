7820d7922470be83e1002904256d9e9df200dfe2
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-11-16 17:53:32 +0300

    upd capture script

--- sphinx-app/bash/smart-home-audio-capture.sh
+++ sphinx-app/bash/smart-home-audio-capture.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 # interrupt recoding after # seconds
 WAITING=5
@@ -11,13 +13,19 @@ if [ ! -d "$NFS_FOLDER" ]; then
 fi
 
 echo "Going to capture audio per $WAITING"
-while [ true ]; do
-    # recording 10 times
-    for i in {1..10}
-    do
-        arecord -f S16_LE -r 8000 -d $WAITING "$NFS_FOLDER/start-signal-$(date +%T).wav"
-    done;
 
-    # delete unneeded files
-    find -name 'start-signal-*.wav' -mtime -1 -exec rm -f {} \;
-done
+function capture_start_signal() {
+    while [ true ]; do
+        # recording 10 times
+        for i in {1..10}
+        do
+            arecord -f S16_LE -r 8000 -d $WAITING "$NFS_FOLDER/start-signal-$(date +%T).wav"
+        done;
+
+        # delete unneeded files
+        find $NFS_FOLDER -name 'start-signal-*.wav' -mtime -1 -exec rm -f {} \;
+    done
+}
+export -f wait_start
+
+capture_start_signal
