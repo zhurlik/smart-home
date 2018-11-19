51781b9ac1ecdf87b5b6dea5c9579849bbf7c4c1
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2018-11-19 12:01:17 +0300

    fix travis-ci

--- sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/ApplicationTest.java
+++ sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/ApplicationTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
@@ -80,8 +81,17 @@ public class ApplicationTest {
         // reading a few times a wav file
         executorService.submit(() -> {
             while (!stop.get()) {
-                try (final InputStream fileIn = new BufferedInputStream(wav.getInputStream())){
-                     out.write(fileIn.readAllBytes());
+                try (final InputStream is = wav.getInputStream()) {
+                    final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                    int nRead;
+                    final byte[] data = new byte[1024];
+                    while ((nRead = is.read(data, 0, data.length)) != -1) {
+                        buffer.write(data, 0, nRead);
+                    }
+
+                    buffer.flush();
+                    final byte[] byteArray = buffer.toByteArray();
+                    out.write(byteArray);
                 } catch (IOException e) {
                     LOGGER.error(">> Error during reading audio file:", e);
                 }
