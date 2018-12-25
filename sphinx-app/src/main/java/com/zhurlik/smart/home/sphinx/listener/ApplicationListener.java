d9974bdc8c35769a4c93a91a5e8e4ed11d86fa15
zhurlik <zhurlik@gmail.com>
2018-12-25 15:00:32 +0300

    main app listener

---
+++ sphinx-app/src/main/java/com/zhurlik/smart/home/sphinx/listener/ApplicationListener.java
@@ -0,0 +1,33 @@
+package com.zhurlik.smart.home.sphinx.listener;
+
+import com.zhurlik.smart.home.sphinx.event.AudioScannerEvent;
+import com.zhurlik.smart.home.sphinx.event.SpeechRecognizerEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Main listener for handling application events for starting/stopping other listeners.
+ *
+ * @author zhurlik@gmail.com
+ */
+@Component
+public class ApplicationListener {
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    @EventListener
+    public void up(final ContextRefreshedEvent event) {
+        eventPublisher.publishEvent(SpeechRecognizerEvent.START);
+        eventPublisher.publishEvent(AudioScannerEvent.FETCH);
+    }
+
+    @EventListener
+    public void down(final ContextClosedEvent event) {
+        eventPublisher.publishEvent(SpeechRecognizerEvent.STOP);
+    }
+}
---
+++ sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/listener/ApplicationListenerTest.java
@@ -0,0 +1,45 @@
+package com.zhurlik.smart.home.sphinx.listener;
+
+import com.zhurlik.smart.home.sphinx.event.AudioScannerEvent;
+import com.zhurlik.smart.home.sphinx.event.SpeechRecognizerEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationListenerTest {
+
+    @InjectMocks
+    private ApplicationListener listener;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @Test
+    void testUp() {
+        // Given, When
+        listener.up(any(ContextRefreshedEvent.class));
+
+        // Then
+        verify(eventPublisher).publishEvent(SpeechRecognizerEvent.START);
+        verify(eventPublisher).publishEvent(AudioScannerEvent.FETCH);
+    }
+
+    @Test
+    void testDown() {
+        // Given, When
+        listener.down(any(ContextClosedEvent.class));
+
+        // Then
+        verify(eventPublisher).publishEvent(SpeechRecognizerEvent.STOP);
+    }
+}
