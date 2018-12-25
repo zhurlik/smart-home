d2e5ca48630dc25544dd0c6b416d1bb77d93ab8b
zhurlik <zhurlik@gmail.com>
2018-12-25 18:50:54 +0300

    simplify unit tests

--- sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/config/SphinxConfigTest.java
+++ sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/config/SphinxConfigTest.java
@@ -2,60 +2,43 @@ package com.zhurlik.smart.home.sphinx.config;
 
 import edu.cmu.sphinx.api.Configuration;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 /**
  * Unit tests that use mocks and fake spring context.
  *
  * @author zhurlik@gmail.com
  */
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SphinxConfig.class)
-@TestPropertySource(properties = {
-        "sphinx.use.grammar=true",
-        "sphinx.grammar.path=test-grammar-path",
-        "sphinx.acoustic.model.path=acoustic-test-path",
-        "sphinx.dictionary.path=test-dict"
-})
-public class SphinxConfigTest {
-
-    @Autowired
-    private Configuration sphinx;
-
-    @Value("${user.dir}/zero_ru_cont_8k_v3/ru.lm")
-    private String expectedRuLm;
+class SphinxConfigTest {
+    private final SphinxConfig config = new SphinxConfig();
 
     @Test
     void testMain() {
+        final Configuration sphinx = config.sphinx();
         assertNotNull(sphinx);
-        assertTrue(sphinx.getUseGrammar());
-        assertEquals("smart-home", sphinx.getGrammarName());
-        assertEquals("test-grammar-path", sphinx.getGrammarPath());
-        assertEquals("acoustic-test-path", sphinx.getAcousticModelPath());
-        assertEquals(expectedRuLm, sphinx.getLanguageModelPath());
-        assertEquals("test-dict", sphinx.getDictionaryPath());
-        assertEquals(8000, sphinx.getSampleRate());
+        assertFalse(sphinx.getUseGrammar());
+        assertNull(sphinx.getGrammarName());
+        assertNull(sphinx.getGrammarPath());
+        assertNull(sphinx.getAcousticModelPath());
+        assertNull(sphinx.getLanguageModelPath());
+        assertNull(sphinx.getDictionaryPath());
+        assertEquals(0, sphinx.getSampleRate());
     }
 
     @Test
-    void testDefault() {
-        final Configuration conf = new SphinxConfig().sphinx();
-        assertFalse(conf.getUseGrammar());
-        assertNull(conf.getGrammarName());
-        assertNull(conf.getGrammarPath());
-        assertNull(conf.getAcousticModelPath());
-        assertNull(conf.getLanguageModelPath());
-        assertNull(conf.getDictionaryPath());
+    void testBeans() throws IOException {
+        final Configuration mockConfiguration = mock(Configuration.class);
+
+        assertThrows(NullPointerException.class, () -> config.streamSpeechRecognizer(mockConfiguration));
+        assertNotNull(config.speechIn());
+        assertNotNull(config.speechOut(config.speechIn()));
     }
 }
