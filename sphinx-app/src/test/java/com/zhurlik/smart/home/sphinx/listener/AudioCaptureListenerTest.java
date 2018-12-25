e1f17f6ea011c0e031b6832e538ba1830c9ab42c
zhurlik <zhurlik@gmail.com>
2018-12-25 22:00:11 +0300

    added unit tests

---
+++ sphinx-app/src/test/java/com/zhurlik/smart/home/sphinx/listener/AudioCaptureListenerTest.java
@@ -0,0 +1,45 @@
+package com.zhurlik.smart.home.sphinx.listener;
+
+import com.zhurlik.smart.home.sphinx.event.AudioScannerEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class AudioCaptureListenerTest {
+
+    @InjectMocks
+    private AudioCaptureListener listener;
+
+    @Spy
+    private Resource testDir = new ClassPathResource("test-audio");
+
+    @Spy
+    private PipedOutputStream out = new PipedOutputStream();
+
+    @Test
+    void testScan() throws Exception {
+        final PipedInputStream in = new PipedInputStream();
+        out.connect(in);
+
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> listener.onApplicationEvent(AudioScannerEvent.START));
+
+        TimeUnit.SECONDS.sleep(3);
+
+        listener.onApplicationEvent(AudioScannerEvent.STOP);
+        assertEquals("first fake audio filesecond fake audio file", new String(in.readAllBytes()));
+    }
+}
