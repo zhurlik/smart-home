aa23e0417db527de03d2bf6b7ff237c1b2dc0f1b
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2019-10-28 16:43:24 +0300

    added HtmlWebpackPlugin

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,11 @@ module.exports = {
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Smart Home'
+        })
+    ]
 };
