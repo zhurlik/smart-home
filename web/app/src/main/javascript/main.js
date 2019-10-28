9985c0ef9ad11e77b378ac0fbf1040cbb00e36f5
zhurlik <zhurlik@gmail.com>
2019-10-28 21:59:43 +0300

    stomp client

--- web/app/src/main/javascript/main.js
+++ web/app/src/main/javascript/main.js
@@ -1,4 +1,17 @@
 const Stomp = require('stompjs');
-const client = Stomp.client('ws://localhost:61614');
+const client = Stomp.client('ws://192.168.100.3:15674/ws');
+
+let on_connect = function (x) {
+    id = client.subscribe("/topic/test", function (d) {
+        console.log('>>' + d);
+    });
+};
+
+let on_error = function () {
+    console.log('>> Error');
+};
+
+client.connect('smart-home', 'smart-home', on_connect, on_error, '/');
+
 
 document.write("Smart Home");
