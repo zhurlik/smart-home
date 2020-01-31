70a5aeea597d3d4abd4cf6692ae61460fb68c9bc
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-01-31 10:39:27 +0300

    shell script for starting the docker container with rabbitmq

---
+++ mqtt-broker/src/main/shell/up-rabbitmq-servers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://hub.docker.com/_/rabbitmq
+
+docker --version
+docker image ls
+docker build --no-cache --tag test-rabbitmq ./src/main/resources/dockerfiles/
+docker ps -a
+docker container stop test-rabbitmq-server
+docker rm -f test-rabbitmq-server
+docker run -d --hostname test-rabbitmq-server --name test-rabbitmq-server -p 15672:15672 -p 5672:5672 -p 15674:15674 test-rabbitmq
+
+# SSH login docker exec -it test-rabbitmq-server /bin/bash
