b68fb2f04954077407741d6a32abdc8d50895889
Uladzimir Zhuraulevich <uzhuraulevich@copyright.com>
2020-02-03 11:42:44 +0300

    added mqtt port

--- mqtt-broker/src/main/shell/up-rabbitmq-servers.sh
+++ mqtt-broker/src/main/shell/up-rabbitmq-servers.sh
@@ -8,6 +8,6 @@ docker build --no-cache --tag test-rabbitmq ./src/main/resources/dockerfiles/
 docker ps -a
 docker container stop test-rabbitmq-server
 docker rm -f test-rabbitmq-server
-docker run -d --hostname test-rabbitmq-server --name test-rabbitmq-server -p 15672:15672 -p 5672:5672 -p 15674:15674 test-rabbitmq
+docker run -d --hostname test-rabbitmq-server --name test-rabbitmq-server -p 15672:15672 -p 5672:5672 -p 15674:15674 -p 1883:1883 test-rabbitmq
 
 # SSH login docker exec -it test-rabbitmq-server /bin/bash
